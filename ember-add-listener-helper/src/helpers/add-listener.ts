507282d5d2b1eede904ea7991af58faa7823416f
Ben Demboski <demboskb@gmail.com>
2023-07-23 15:32:48 -0700

    Typescriptify

---
+++ ember-add-listener-helper/src/helpers/add-listener.ts
@@ -0,0 +1,52 @@
+import Helper from '@ember/component/helper';
+import { addListener, removeListener } from '@ember/object/events';
+
+// We define these explicitly rather than using `Parameters<typeof addListener>`
+// because `addListener`'s arguments were typed incorrectly in earlier preview
+// versions of Ember types.
+type PositionalArgs = [
+  obj?: object,
+  eventName?: string,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  target?: object | Function | null,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  method?: Function,
+  sync?: boolean
+];
+
+interface AddListenerHelperSignature {
+  Args: {
+    Positional: PositionalArgs;
+  };
+  Return: undefined;
+}
+
+export default class AddListenerHelper extends Helper<AddListenerHelperSignature> {
+  private removeListener?: () => void;
+
+  compute(args: PositionalArgs) {
+    this.checkRemoveListener();
+
+    const [obj, eventName, target, method, once] = args;
+    // method and once are optional
+    if (!obj || !eventName || !target) {
+      return undefined;
+    }
+
+    addListener(obj, eventName, target, method, once);
+    this.removeListener = () => removeListener(obj, eventName, target, method);
+    return undefined;
+  }
+
+  willDestroy() {
+    this.checkRemoveListener();
+    super.willDestroy();
+  }
+
+  checkRemoveListener() {
+    if (this.removeListener) {
+      this.removeListener();
+      delete this.removeListener;
+    }
+  }
+}
